e0ee82ebe0b6c35197c20f9ec601b753a9a1d80e
shawn-goh24 <ahshawngoh@gmail.com>
2023-05-12 17:41:39 +0800

    Include add/edit/delete budget into controller

--- controllers/BudgetsController.js
+++ controllers/BudgetsController.js
@@ -1,42 +1,83 @@
 const db = require("../db/models/index");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
-const { Budget } = db;
+const { Budget, Category } = db;
 
 async function getAllBudget(req, res) {
+  const { userId, month, year } = req.params;
   try {
-    const data = await Budget.findAll({});
+    const data = await Budget.findAll({
+      where: {
+        userId: userId,
+        [Op.and]: [
+          Sequelize.where(
+            Sequelize.fn("EXTRACT", Sequelize.literal('MONTH FROM "date"')),
+            month
+          ), // replace 4 with the month number you want to find
+          Sequelize.where(
+            Sequelize.fn("EXTRACT", Sequelize.literal('YEAR FROM "date"')),
+            year
+          ), // replace 2023 with the year you want to find
+        ],
+      },
+      include: Category,
+    });
     return res.json(data);
   } catch (err) {
     return res.status(400).json({ error: true, msg: err });
   }
 }
 
-// async function editBudget(req, res) {
-//   try {
-//     let budgetToAdd = req.body;
-//     let budgetToReplace = req.params.budgetId;
-//     let budgetToEdit = await Budget.findByPk(budgetToReplace);
-//     await budgetToEdit.update(budgetToAdd);
-//     let allBudgets = await Budget.findAll();
-//     return res.json(allBudgets);
-//   } catch (err) {
-//     return res.status(400).json({ error: true, msg: err });
-//   }
-// }
+async function addBudget(req, res) {
+  const { userId, categoryId, amount, date } = req.body;
+  try {
+    const data = await Budget.create({
+      userId: userId,
+      categoryId: categoryId,
+      amount: amount,
+      date: date,
+    });
+    const returnedData = await Budget.findByPk(data.id, {
+      include: Category,
+    });
+    return res.json(returnedData);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
 
-// async function deleteBudget(req, res) {
-//   try {
-//     const { budgetId } = req.params;
-//     await Budget.destroy({ where: { id: budgetId } });
-//     let allBudgets = await Budget.findAll();
-//     return res.json(allBudgets);
-//   } catch (err) {
-//     return res.status(400).json({ error: true, msg: err });
-//   }
-// }
+async function editBudget(req, res) {
+  try {
+    let budgetToAdd = req.body;
+    let budgetToReplace = req.params.budgetId;
+    let budgetToEdit = await Budget.findByPk(budgetToReplace);
+    await budgetToEdit.update(budgetToAdd);
+    let allBudgets = await Budget.findAll({
+      include: Category,
+    });
+    return res.json(allBudgets);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
+
+async function deleteBudget(req, res) {
+  try {
+    const { budgetId } = req.params;
+    await Budget.destroy({ where: { id: budgetId } });
+    let allBudgets = await Budget.findAll({
+      include: Category,
+    });
+    return res.json(allBudgets);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
 
 module.exports = {
   getAllBudget,
-  // editBudget,
-  // deleteBudget,
+  addBudget,
+  editBudget,
+  deleteBudget,
 };
--- routers/BudgetsRouter.js
+++ routers/BudgetsRouter.js
@@ -4,10 +4,11 @@ const budgetController = require("../controllers/BudgetsController");
 const router = express.Router();
 
 // Get all budget
-router.get("/", budgetController.getAllBudget);
+router.get("/:userId/:month/:year", budgetController.getAllBudget);
+router.post("/add", budgetController.addBudget);
 // Edit category
-// router.put("/edit/:categoryId", budgetController.editBudget);
+router.put("/edit/:budgetId", budgetController.editBudget);
 // Delete category
-// router.delete("/delete/:categoryId", budgetController.deleteBudget);
+router.delete("/delete/:budgetId", budgetController.deleteBudget);
 
 module.exports = router;
